cc054c32ec523ca6379788d09b18fdad1bfa73ae
konstr17 <cheezzz@list.ru>
2023-11-08 15:43:05 +0300

    lss3

--- lssn3/arrayBrackets.cpp
+++ lssn3/arrayBrackets.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
+using namespace std;
 
+void push(char stck[], int t, char a)
+{
+    t++;
+    stck[t] = a;
+}
+
+void pop(char stck[], int t, char pr);
+
+int main()
+{
+    char stek[80];
+    int i, n, top, kon;
+    string vir;
+    char a, b, pr;
+}
